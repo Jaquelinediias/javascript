f70b50a627a28e43eaa186f403020793293c0c87
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-15 17:04:37 -0300

    exercicio01 ok

--- exercicios/ex001/script.js
+++ exercicios/ex001/script.js
@@ -1,16 +1,16 @@
-let nome= "Elza";
-let idade= 46;
-let estado= "Bahia";
-let cidade="Salvador";
+let nome = "Jaqueline"; // String 
+let idade = 90 ; // Number
+let logado = true; // boolean
+let list = ['café ','ovo', 'farinha']; // array
+let nomeCompleto = {nome: "Jaqueline", idade: 26} // object
+/* 
+o comando abaixo exibe os dados no Console
 
-console.log(nome)
-console.log(idade)
-console.log (estado)
-console.log(cidade)
+*/
+console.log(typeof list)
 
 
 
-              
 
 
 
