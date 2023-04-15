e1206d67526064110a1f2934509dbe9f1d5043b0
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-15 20:36:39 -0300

    exercicio05 ok

---
+++ exercicios/ex005/script05.js
@@ -0,0 +1,39 @@
+let idade = 45;
+
+if(idade > 17) {
+    console.log("Você é maior de idade.");
+} else {
+    console.log("Você é MENOR de idade.");
+}
+
+// Exemplo02
+
+let idadeI = 30;
+
+if(idadeI >= 18){
+    console.log("Você é MAIOR de idade");
+} else{
+    console.log('Você é MENOR de idade.');
+}
+
+
+
+    /*CONDICIONAIS == E === E SUAS DIFERENÇAS*/
+  
+    let diadoNascimento = "20"
+
+    if(diadoNascimento == 20){ // condicional menos rigorosa
+        console.log('Você nasceu no dia 20');
+    }
+//----------------------------------------------------------
+//----------------------------------------------------------
+    let diadoNascimentoI = 20
+
+    if(diadoNascimentoI === 20){ // condicional maisrigorosa
+        console.log('Você nasceu no dia 20');
+    }
+
+
+
+
+
