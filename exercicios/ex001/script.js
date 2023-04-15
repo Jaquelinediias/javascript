b3ed8f0f75d7ea29518ddf073a6d56fdc882c3f1
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-15 15:50:05 -0300

    Update script.js

--- exercicios/ex001/script.js
+++ exercicios/ex001/script.js
@@ -1,4 +1,15 @@
-alert('óla mundo')
+let nome= "Elza";
+let idade= 46;
+let estado= "Bahia";
+let cidade="Salvador";
+
+console.log(nome)
+console.log(idade)
+console.log (estado)
+console.log(cidade)
+
+
+
               
 
 
