bcbb684d92af0d66f8f62b1d687167d8a7c16b53
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-15 19:18:06 -0300

    exercicio04 ok

---
+++ exercicios/ex004/script004.js
@@ -0,0 +1,22 @@
+let nome ='Robson';
+let sobrenome = 'Santos';
+
+let nomeCompleto = nome + ' ' + sobrenome;
+
+console.log("Nome: " + nomeCompleto);
+
+// Exemplo de Templante String
+
+let nomeI = 'Erick';
+let sobrenomeI = 'Bessa';
+
+let nomeCompletoI = `${nomeI} ${sobrenomeI} Da Luz `;
+
+console.log("Nome:" + nomeCompletoI);
+
+// Exemplo02
+
+let idade = 27;
+let idadeString = `Idade: ${idade} anos`;
+
+console.log(idadeString);
