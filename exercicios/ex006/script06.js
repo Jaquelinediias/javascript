ec28d0690b74e9e1ed804342d416628c8b39984e
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-15 21:22:41 -0300

    exercicio006 ok

---
+++ exercicios/ex006/script06.js
@@ -0,0 +1,22 @@
+    // Exemplo01
+/*
+let idade = 30;
+
+if(idade >= 18) {
+   if(idade < 60){
+     console.log("Você é um adulto");
+   } 
+} 
+*/
+ //--------------------------------------------------------
+          //Exemplo02 multiplicas condições
+
+let idade = 45;
+
+if(idade >= 18 && idade < 60) {
+     console.log("Você é um adulto"); 
+
+} 
+
+
+
