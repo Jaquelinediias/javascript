c03e71d7e03f332f44c7d1c6aaf9f5b26e5c572e
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-17 16:21:41 -0300

    exercicio08 ok

---
+++ exercicios/ex008/script08.js
@@ -0,0 +1,15 @@
+/*
+let idade = 15;
+
+let maiorDeIdade = idade >= 18 && idade < 60;
+
+if(maiorDeIdade) {
+    console.log("É ADULTO");
+} else{
+    console.log("Não é Adulto.");
+}
+*/
+   // Exemplo02
+
+let idade = 10;
+ console.log(idade > 18);
