6d8aed34daa288b0a422dcf707a4bf7911ef9ca8
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-17 19:48:37 -0300

    Update script09.js

---
+++ exercicios/ex009/script09.js
@@ -0,0 +1,41 @@
+// Substitua o @ pela condicional correta para que
+// o resultado seja TRUE
+
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+// Substitua o @ pela condicional correta para que
+// o resultado seja FALSE
+
+let w = "10";
+let z = 10;
+console.log(w === z);
+
+// Substitua o @ pela condicional correta para que 
+// o resultado seja TRUE
+
+let a = 10;
+let b = 5;
+console.log(x != y);
+
+// Crie uma condicional para verificar se o preço da
+// Carne está barata ou caro. Ps: Até 45$ está barato.
+
+let preco = 40.3
+
+if(preco <= 45) {
+    console.log("A carne está barata");
+} else {
+    console.log("A carne está cara");
+}
+
+//--------------------------------
+
+let valordoPC = 1.200
+
+if(valordoPC >= 1.500){
+    console.log ("Compra o PC");
+} else{
+    console.log("Não comprar #Triste")
+}
