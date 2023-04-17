e8cd17e43b5f3e4cb0c1c6dd6f52b3fc559481d6
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-17 14:20:58 -0300

    exercicio07 ok

--- exercicios/ex007/script07.js
+++ exercicios/ex007/script07.js
@@ -1,1 +1,29 @@
-let 
+  /* Condicionais Independentes*/
+/*
+let idade = 85;
+
+if(idade < 18){
+    console.log('Você é uma criança.');
+}
+
+if(idade >= 18 && idade <= 60) {
+    console.log("Você é um adulto");
+}
+
+if(idade >= 60){
+    console.log('Você é um Idoso (a).');
+}
+*/
+  
+  // Exemplo de Condicionais Dependentes.
+  
+let idade = 95;
+
+if(idade < 18){
+    console.log('Você é uma criança.');
+} else if (idade >= 18 && idade <= 60) {
+    console.log("Você é um adulto");
+} else if (idade >= 60) {
+    console.log('Você é um Idoso (a).');
+}
+
