52349d002ae091e9bf91a9599dcd5895e55eec15
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-21 13:58:26 -0300

    exercicio010 ok

---
+++ exercicios/ex010/script010.js
@@ -0,0 +1,15 @@
+let isMember = true;
+
+let shipping = isMember ? 2 : 10;
+
+console.log(isMember? 'Você é um Membro valor do 2$' :'Você não é um Membro valor do 10$');
+console.log("frete: " + shipping);
+
+//-------------------------------------//
+
+//Exemplo 02//
+
+let age = 90;
+
+let isAdult = ( age >= 18? 'yes' : 'No');
+console.log(isAdult);
