a7f2d5214ca5bbd910f2e7e3ee0d061b02d65650
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-21 15:06:09 -0300

    exercicio011 OK

---
+++ exercicios/ex011/script011.js
@@ -0,0 +1,18 @@
+let profession = "policial";
+
+console.log("profissão: "  +  profession);
+
+switch (profession) {
+    case 'Biólogo':
+        console.log('Your shirt will be GREEN');
+        break;
+    case 'bombeiro':
+        console.log('Your shirt will be RED');
+        break;
+    case 'policial':
+        console.log('Your shirt will be BLUE');
+        break;
+    default:
+        console.log('Your shirt will be BLACK');
+        break;
+}
