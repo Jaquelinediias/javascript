97228517b9c8dbfa54beb214a2042b805a28c6cb
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-22 18:23:38 -0300

    exercicio ok

---
+++ exercicios/ex013/script013.js
@@ -0,0 +1,19 @@
+/*
+function somar(x, y) {
+    return x + y;
+}
+*/ 
+//ARROW FUNCTION 01
+/*
+const somar = (x, y) => {
+    return x + y;
+}
+*/
+// ARROW FUNCTION 02
+
+const somar = (x, y) => x + y;
+
+console.log(somar(10, 5));
+
+
+/* () => {}  Isso é uma arrow function*/
