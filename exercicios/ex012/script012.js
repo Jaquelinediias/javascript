f6eba0d820d7a5008b63654899ec5b13732321bb
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-22 16:20:26 -0300

    exercício concluido

--- exercicios/ex012/script012.js
+++ exercicios/ex012/script012.js
@@ -44,3 +44,87 @@ if (verificacao) {
 } else {
     console.log('É MENOR de idade');
 }
+//------------------------------------------------
+//------------------------------------------------
+
+//EXERCICIO DE FUNÇÕES.
+
+/*calcule a porcentagem entre 2 números.
+Exemploo: 25% de 40 é 10
+Fórmula da porcentagem: (y / x) * 100
+Uso da Função:
+*/
+
+function calcPct(n1, n2) {
+    let pct = (n2 / n1) * 100;
+    return pct;
+}
+
+let x = 40;
+let y = 10;
+let pct = calcPct(x, y);
+console.log(`${pct}% de ${x} é ${y}`);
+
+//-----------------------------------------------------
+//-----------------------------------------------------
+     //EXERCÍCIO DE FUNÇÕES 02.
+/*
+CALCULE O PRECO DO IMÓVEL
+- M2 = 3.000
+- SE TIVER 1 QUARTO, O M2 É 1X
+- SE TIVER 2 QUARTOS, O M2 É 1.2X
+-SE TIVER 3 QUARTOS, O M2 É 1.5X
+USO DA FUNÇÃO:
+*/
+
+function calcularImovel(metragem, quartos){
+    let m2 = 3000;
+    let preco = 0;
+
+    switch (quartos) {
+        case 1:
+        default:
+            preco = metragem * m2;
+            break;
+        case 2:
+            preco = metragem * (m2 * 1.2);
+            break;
+        case 3:
+            preco = metragem * (m2 * 1.5);
+            break; 
+    }
+    return preco;
+}
+
+let metragem = 123;
+let quartos = 5;
+let preco = calcularImovel(metragem, quartos);
+console.log(`A casa custa R$ ${preco}`);
+
+//-----------------------------------------
+    /*Exercicio 03*/
+//-----------------------------------------
+/*
+CRIE UMA FUNÇÃO QUE VALIDE USUÁRIO E SENHA.
+USUÁRIO CORRETO: Playbozinho
+SENHA CORRETA: 123
+*/
+
+function validar ( usuario, senha ){
+   if(usuario === 'Playboizinho' && senha === '123'){
+       return true;
+   } else {
+     return false;
+   }
+}
+let usuario = "Thor Deus do Trovão";
+let senha = '123';
+let validacao = validar(usuario, senha);
+if(validacao) {
+    console.log('Acesso permitido.')
+} else {
+    console.log('Acessa NEGADO!');
+}
+
+
+ 
