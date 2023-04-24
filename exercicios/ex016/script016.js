e1206cc64863e20fdf8cb61ca42abe542664fcfd
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-24 13:57:13 -0300

    pasta016 criada

---
+++ exercicios/ex016/script016.js
@@ -0,0 +1,21 @@
+let colors = ['Blue','Red','Green'];
+
+console.log(colors[0]);
+//-----------------------------------
+//-----------------------------------
+//   EXEMPLO 02 //
+
+let clientesBolos = ['jaqueline', 'robson','Elza'];
+let lista = ['leite', 20, 'ovos', true];
+
+console.log(clientesBolos, lista)
+
+//------------------------------------
+//------------------------------------
+    // EXEMPLO 03//
+
+    let formula = ['h2o',['xx', 'yy']];
+
+    console.log(formula[1][0]);
+
+
