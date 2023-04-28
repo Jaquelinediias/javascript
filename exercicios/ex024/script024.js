240d19e7ad8cfb216aa24377dc483448affb1500
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-04-28 13:50:33 -0300

    concluído

---
+++ exercicios/ex024/script024.js
@@ -0,0 +1,21 @@
+ let fruits = ['Maça', 'Uva', 'Laranja', 'Banana'];
+
+ fruits[fruits.length -1] = 'Acerola'
+
+ fruits.pop();
+ fruits.push('Acerola');
+ 
+ console.log(fruits);
+
+
+/*
+fruits.pop();
+fruits.shift();
+*/
+
+
+ /*
+ fruits.push('pêra');
+ console.log(fruits)
+ console.log(fruits.length);
+ */
