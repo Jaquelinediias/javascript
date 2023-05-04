43f22fff63154fa162fdefc31e6616e66e4d71bb
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-05-04 17:13:08 -0300

    exercício ok

---
+++ exercicios/ex034/script034.js
@@ -0,0 +1,18 @@
+function soltou(e) {
+    console.log('Aperta tecla' + e.code);
+    console.log('SHIFT ?' + e.shiftKey);
+    console.log('--');
+
+}
+
+const input = document.querySelector('input');
+input.addEventListener('keyup', soltou);
+
+
+
+
+
+
+
+/*KeyPress, KeyDown e KeyUp são eventos acionados pelo teclado. O KeyDown acontece primeiro (quando a tecla esta abaixando). O KeyPress acontece segundo (quando o texto for digitado). O KeyUp acontece depois da tecla digitada (quando a tecla estiver subindo e quando a entrada de texto está completo)
+*/
