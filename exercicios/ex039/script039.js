5a04d01c664340b3f2dadb34b627158e2c088702
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-05-08 19:07:39 -0300

    novas atualizações

--- exercicios/ex039/script039.js
+++ exercicios/ex039/script039.js
@@ -1,7 +1,31 @@
  class Person {
    age = 0;
 
-   constructor(name){
+   constructor(name) {
     this.name = name;
    }
+   sayHi(){
+    console.log(`${p1.name} Diz Oi`)
+   }
 }
+
+class Student extends Person{
+
+   constructor(name, id) {
+    super(name);
+    this.id = id;
+   }
+   sayHello(){
+    console.log(`${p1.age} tem anos`)
+   }
+}
+
+let p1 = new Student("Robson", 555);
+//Para seta a idade do estudante.
+p1.age = 22;
+
+p1.sayHello()
+p1.sayHi()
+/*
+console.log(`${p1.name} tem ${p1.age} anos. Número da Matricula é: #${p1.id}`)
+*/
