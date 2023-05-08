8a3433b8d0a80977e8521f0cd7e15a76d489d977
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-05-08 20:36:50 -0300

    concluido

---
+++ exercicios/ex040/script040.js
@@ -0,0 +1,16 @@
+class Person{
+    age = 0;
+
+    constructor(name){
+     this.name = name;
+    }
+}
+function createPerson(name, age) {
+    let p = new Person(name);
+    p.age = age;
+    return p;
+
+    
+}
+let p1 = createPerson("Robson", 22);
+console.log(`${p1.name} tem ${p1.age} anos.`);
