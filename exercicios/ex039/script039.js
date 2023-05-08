c9468eeb9babe80f0cf40f7d4530a97722727a54
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-05-08 20:15:54 -0300

    substituição de exercício

--- exercicios/ex039/script039.js
+++ exercicios/ex039/script039.js
@@ -1,31 +1,16 @@
  class Person {
+
+   static hands = 2;
    age = 0;
-
-   constructor(name) {
-    this.name = name;
-   }
-   sayHi(){
-    console.log(`${p1.name} Diz Oi`)
-   }
-}
-
-class Student extends Person{
-
-   constructor(name, id) {
-    super(name);
-    this.id = id;
+   
+   constructor (name) {
+      this.name = name;
    }
    sayHello(){
-    console.log(`${p1.age} tem anos`)
+      console.log(`Olá, eu sou ${this.name} e eu tenho ${Person.hands} mãos`)
    }
-}
-
-let p1 = new Student("Robson", 555);
-//Para seta a idade do estudante.
-p1.age = 22;
-
-p1.sayHello()
-p1.sayHi()
-/*
-console.log(`${p1.name} tem ${p1.age} anos. Número da Matricula é: #${p1.id}`)
-*/
+ }
+ let p2 = new Person ("Robson");
+ let p1 = new Person("Erick");
+ p1.sayHello();
+ p2.sayHello();
