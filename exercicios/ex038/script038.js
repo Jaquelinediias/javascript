872553682c28b38292acee0dfdc606dc271415dc
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-05-08 19:10:01 -0300

    novo exercício

--- exercicios/ex038/script038.js
+++ exercicios/ex038/script038.js
@@ -21,3 +21,37 @@ let p1 = new Student("Laura", 5846);
 p1.age = 18;
 
 p1.helloHi();
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Person {
+    age = 0;
+ 
+    constructor(name) {
+     this.name = name;
+    }
+    sayHi(){
+     console.log(`${p1.name} Diz Oi`)
+    }
+ }
+ 
+ class Student extends Person{
+ 
+    constructor(name, id) {
+     super(name);
+     this.id = id;
+    }
+    sayHello(){
+     console.log(`${p1.age} tem anos`)
+    }
+ }
+ 
+ let p1 = new Student("Robson", 555);
+ //Para seta a idade do estudante.
+ p1.age = 22;
+ 
+ p1.sayHello()
+ p1.sayHi()
+ /*
+ console.log(`${p1.name} tem ${p1.age} anos. Número da Matricula é: #${p1.id}`)
+ */
