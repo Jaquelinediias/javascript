7e5a2d5fdd2ff031a50eccc2ffa3635a636e4c9e
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-05-31 14:42:04 -0300

    Update script42.js

---
+++ exercicios/ex042/script42.js
@@ -0,0 +1,16 @@
+/* como a web funciona(Requisição e Resposta)?
+Request = Requisção
+--> Cabeçalhos / Header
+--> Corpo / Body
+
+
+Response = Respota 
+devolver reposta.
+*/
+
+/* Como Funciona uma API*
+Api = Comuicação de um software = celular, geladeira, etc.
+
+
+JSON = Javascript Object Notation
+/
