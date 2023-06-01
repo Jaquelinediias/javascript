4dc3b409e2f30823487749aec35f7f8faa8aa6ba
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-06-01 00:07:18 -0300

    ...

--- exercicios/ex042/script42.js
+++ exercicios/ex042/script42.js
@@ -13,4 +13,10 @@ Api = Comuicação de um software = celular, geladeira, etc.
 
 
 JSON = Javascript Object Notation
-/
+*/
+
+///Callback? função que criopara ser executada no  futuro
+
+document.querySelector("#botao").addEventListener('click', () => {
+    alert("clicou no Botao")
+})
