b8e587fd66e512225d2690bf810a2f5dcb680cd3
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-06-03 20:41:34 -0300

    novo exercício criado

--- exercicios/ex042/script42.js
+++ exercicios/ex042/script42.js
@@ -1,40 +1,36 @@
-/* como a web funciona(Requisição e Resposta)?
-Request = Requisção
---> Cabeçalhos / Header
---> Corpo / Body
+// let clicou = async () => {
 
+async function clicou() {
 
-Response = Respota 
-devolver reposta.
-*/
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await response.json()
+    alert(`Titulo do primeiro post ${json[0].title}`);
 
-/* Como Funciona uma API*
-Api = Comuicação de um software = celular, geladeira, etc.
-
-
-JSON = Javascript Object Notation
-*/
-
-///Callback? função que criopara ser executada no  futuro
-
-function clicou() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => {
-        console.log(`Status:${response.status}`)
-        return response.json();
-    })
-    .then((json) => {
-        alert(`Clicou no primeiro post:${json[0].title}`)
-    })
-    .catch((error) => {
-        alert("Algo está errado.")
-    })
-    .finally(() =>{
-        alert("Tudo OK, acesse a página")
-
-    })
-   
+    alert("Clicou");
 }
 
-document.querySelector('#botao').addEventListener('click', clicou)
+function inserir() {
+    fetch('https://jsonplaceholder.typicode.com/posts', 
+        {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title:'Titulo de teste',
+                body:'Corpo de teste',
+                userId: 2
+            })
+        })
+    .then((Response) => {
+        return Response.json();
+    })
+    .then((json) => {
+        console.log(json);
+    });
+}
+
+document.querySelector('#botao').addEventListener('click', clicou);
+document.querySelector('#inserir').addEventListener('click', inserir);
+
 
