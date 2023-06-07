79e7c83e276d9b0ca295976c57592201db974718
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-06-07 18:34:16 -0300

    concluido

--- exercicios/ex002/script02.js
+++ exercicios/ex002/script02.js
@@ -1,9 +1,17 @@
  const troca = document.querySelector(".troca");
+ var listaDeCores = ['blue', 'red', 'green'];
+ var index = 0;
  
-
 function clicou(color) {
     troca.style.color = color; 
-   
+    document.querySelector("p").innerHTML = "Senhoras(o)"
 
-    //corpo.style.background = "#cccc"
+    if(index +1 == listaDeCores.length){
+        index = 0;
+    }else{
+        index = index + 1;
+    }
+    
+    troca.textContent = listaDeCores[index]
+
 }
