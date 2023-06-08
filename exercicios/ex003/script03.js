6062d5919d725fada4cce921d4dd98531da2ca93
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-06-07 21:05:43 -0300

    atualizada

--- exercicios/ex003/script03.js
+++ exercicios/ex003/script03.js
@@ -1,26 +1,6 @@
-let x = 2;
-
-x = x + 10;
-console.log(x);
-
-// exemplo002
-let n1 = 15;
-let n2 = 35;
-let n3 = 3;
-
-let soma = (n1 * n2) + n3;
-console.log (soma)
-
-// exemplo de concatenação básica
-
-let nome = "Elza";
-let sobrenome = "Conceição";
-
-let nomeCompleto = nome + ' ' + sobrenome;
-
-console.log(nomeCompleto);
-
- 
+function digitou() {
+  console.log("você digitou algo")
+}
 
 
 
