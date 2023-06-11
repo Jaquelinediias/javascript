3a47888d047c8ad4fdad17f3f58e481eded19c9c
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-06-11 20:27:46 -0300

    .

--- exercicios/ex002/script02.js
+++ exercicios/ex002/script02.js
@@ -1,27 +1,3 @@
- /*function blue(){
-    limpar()
-    document.getElementById("troca").classList.add('blue')
-    document.querySelector("p").innerHTML = "Azul"
- }
- function red(){
-    limpar()
-    document.getElementById("troca").classList.add('red')
-    document.querySelector("p").innerHTML = "vermelho"
- }
- function green(){
-    limpar()
-    document.getElementById("troca").classList.add('green')
-    document.querySelector("p").innerHTML = "verde"
- }
-
- function limpar(){
-    document.getElementById("troca").classList.remove('blue')
-    document.getElementById("troca").classList.remove('red')
-    document.getElementById("troca").classList.remove('green')
- }
-*/
-
-function mostrar(elemento) {
-    elemento.style.display ='none';
-    document.getElementById("telefone").style.display = "block";
+function blue() {
+   document.getElementsByClassName('blue').innerText = "Azul"
 }
