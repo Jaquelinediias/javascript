d308f288b4e6565cd50364dad727f14d61cb8473
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-06-21 14:23:04 -0300

    .

--- exercicios/ex005/script05.js
+++ exercicios/ex005/script05.js
@@ -1,38 +1,21 @@
-let idade = 45;
 
-if(idade > 17) {
-    console.log("Você é maior de idade.");
-} else {
-    console.log("Você é MENOR de idade.");
-}
-
-// Exemplo02
-
-let idadeI = 30;
-
-if(idadeI >= 18){
-    console.log("Você é MAIOR de idade");
-} else{
-    console.log('Você é MENOR de idade.');
-}
+let exs = [
+'Bruna',
+ 'Aline', 
+ 'Ana',
+  'Aline', 
+  'Luana'
+ ];
+console.log(exs[3])
 
 
+let carros = {
+  nome: 'Argo',
+  marca: 'Fiat',
+  ano: 2015
+};
 
-    /*CONDICIONAIS == E === E SUAS DIFERENÇAS*/
-  
-    let diadoNascimento = "20"
-
-    if(diadoNascimento == 20){ // condicional menos rigorosa
-        console.log('Você nasceu no dia 20');
-    }
-//----------------------------------------------------------
-//----------------------------------------------------------
-    let diadoNascimentoI = 20
-
-    if(diadoNascimentoI === 20){ // condicional maisrigorosa
-        console.log('Você nasceu no dia 20');
-    }
-
+console.log("Modelo:"+carros.marca)
 
 
 
