7d541d4419606bb296c39e70b4931db7aaf103a7
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-06-25 20:39:01 -0300

    concluído exercício

--- exercicios/ex008/script08.js
+++ exercicios/ex008/script08.js
@@ -1,9 +1,21 @@
 var menuItem = document.querySelectorAll('.item-menu')
-// Guardou todos os elementosque possui uma class item-menu.
+// Guardar todos os elementos que possui uma class item-menu.
 
 function select(){
     menuItem.forEach((item)=>
-      item.classList.remove('ativo') // remove uma class de um item
-
+      item.classList.remove('ativo') // remove uma class de um item.
     )
+    this.classList.add('ativo')
 }
+menuItem.forEach((item) =>
+   item.addEventListener('click', select)
+)
+
+// expandir o menu
+
+var btnExp = document.getElementById('btn-expandir')
+var navMenu = document.querySelector('.menu_lateral')
+
+btnExp.addEventListener('click',function(){
+  navMenu.classList.toggle('expandir')
+})
