456217639f0550b9546d4280e326fc65257f497f
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-06-28 19:05:47 -0300

    novo exercício

--- exercicios/ex010/script010.js
+++ exercicios/ex010/script010.js
@@ -1,15 +1,53 @@
-let isMember = true;
+/*var c = 1
+while(c <= 200){
+   console.log('tudo na paz?')
+   c++
+}
+*/
+/*
+var c = 1;
+let html = ''
 
-let shipping = isMember ? 2 : 10;
+for(var c = 1; c <= 2; c++){
+   html+= "Numero:" +c+ "<br/>"
+}
+document.getElementById("ativo").innerHTML = html;
+*/
 
-console.log(isMember? 'Você é um Membro valor do 2$' :'Você não é um Membro valor do 10$');
-console.log("frete: " + shipping);
 
-//-------------------------------------//
+// foor loop
+// 1 cria variável auxiliares.
+// 2 cria um condição seja satisfeita.
+//3 incrementação
+/*let texto = '';
+for(let c = 0; c < 50; c++){
+    texto = texto + c + "<br/>";
+}
+document.getElementById('ativo'). innerHTML = texto;
+*/
 
-//Exemplo 02//
+// For Loo Array
 
-let age = 90;
+//let carros = ['Palio', 'Fiat', 'Fusca', 'bicicleta'];
+//let html = '<ul>';
 
-let isAdult = ( age >= 18? 'yes' : 'No');
-console.log(isAdult);
+//for(let i in carros ){
+    //html+= '<li>'+ carros[i] + '</li>';
+//}
+//html +='</ul>'
+//document.getElementById('ativo').innerHTML = html;
+
+
+/*for(let i = 0; i <= 10; i++){
+   if(i % 2 !== 0){
+    console.log(i)
+   }
+}
+
+function aperta(){
+    document.querySelector('h1.troca').innerHTML ="For Loop"
+    document.querySelector('body').style.background = "pink"
+    
+}
+*/
+
