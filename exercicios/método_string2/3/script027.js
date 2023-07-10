30e57d87ced02173b83a7eef64ae9e127ff2f577
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-07-10 19:38:40 -0300

    concluído exercício

---
+++ exercicios/método_string2/3/script027.js
@@ -0,0 +1,11 @@
+let nome = 'Jõao Dias Silveira'
+
+let resultado =nome.substr(0, 7);
+
+
+//substring(2, 10)// permite pega a uma string, porem com a limitação de não pega do final pro inicio ex: -5
+
+
+//slice(5, 12) // pega um pedaço da string até onde você quer, sendo ela da frente pro final ou do final pra frente.
+
+console.log(resultado)
