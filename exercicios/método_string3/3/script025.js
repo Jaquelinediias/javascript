fda8aad73ffc55a5756d2fbc8fa993e2d2b021ec
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-07-10 20:26:56 -0300

    atividade finalizada

---
+++ exercicios/método_string3/3/script025.js
@@ -0,0 +1,22 @@
+let nome = ' Jaqueline Dias Silveira Martins'
+let resultado = nome.
+
+//split(' ')Função que transforma uma string em uma array 
+
+
+//charAt(2)Eu quero saber qual é o caractere que está na posição 2.
+
+
+//trim()Função que removeespaço vazios tanto do começo ou do fim de uma string.
+
+
+//toLocaleLowerCase()Texto em minusculo
+
+
+//toLocaleUpperCase()Função para deixa toda a frase maiúscula .
+
+
+//replace('Dias','Perreira') // substitui texto ou nomes dentro de um string .Recebe dois parâmentros x, y.
+
+
+console.log("Resultado:", resultado)
