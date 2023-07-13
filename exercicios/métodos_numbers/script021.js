4a252d95318c001ffddf2765d901afb4b765a526
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-07-12 22:18:57 -0300

    métodos numbers

---
+++ exercicios/métodos_numbers/script021.js
@@ -0,0 +1,14 @@
+let n = '25.9';
+
+let res = parseFloat(n)+5
+
+//parseFloat vai preserva os decimais do numero,se eventualmente ele tiver.
+
+//parseInt vai transforma uma String em Number.
+
+//toFixed() Formatar o número, pôr quantos decimais iremos querer.
+
+//toString() Transforma Number em String.
+
+console.log(res);
+
