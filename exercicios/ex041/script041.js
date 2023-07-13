c9ee1d41c961eec38c8b8d3148cf30534f4dd4e4
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-07-12 23:14:52 -0300

    Manipulação de array

--- exercicios/ex041/script041.js
+++ exercicios/ex041/script041.js
@@ -1,66 +1,18 @@
+let listaAniversario = ['Bolo', 'Decoração', 'Salgadinhos']
+let listaAniversario2 = ['Convite', 'Brinquedo', 'Garçom']
 
- /*/ Uma factory é uma fabrica de cria coisas. 
- function createPerson(name, lastName, age){   //criar pessoa
-     return{
-        name,
-        lastName,
-        age,
+// sort() Ordena proprio array em ordem alfabetica.
 
-        getfullName() {
-            return `${this.name} ${this.lastName}`;
-        }
-     }
- }
-let person1 = createPerson('Jaqueline', 'Lacerda', 35);
-let person2 = createPerson('Eva','Conceição', 15);
+// reverse() Invente o Arrays.
 
-console.log(person1.getfullName());
-*/
+// concat junta o item original, junta o arrays.
+
+//splice(1, 1) Remove item no Array
+
+listaAniversario.sort()
 
 
 
+let res = listaAniversario
 
-// CONSTRUTOR
-
-/*let person1 = {
-    name: "Brisa",
-    lastName:"silva",
-    age: 45,
-    getFullName(){
-        return`${this.name} ${this.age}`
-    },
-    start() {
-        console.log('deu start ')
-        
-    }
-    
-}
-person1.start();
-console.log(person1.getFullName())
-*/
-
-// FUNCIONAL HERANÇA:
-
-const defaultUser = { //objeto padrão]
-    name: '',
-    email: '',
-    level: 1,
-    date: 08,
-}
- 
-let user1 = {
-    ...defaultUser,
-    name: 'Rosi',
-    email: 'jaquelinedias@.gmail.com'
-}
-console.log(user1);
-
-let adm1 = {
-    ...defaultUser,
-    name: 'Adm chapadão',
-    email: 'adm@.com.br',
-   
-}
-
-console.log(adm1)
-
+console.log(res)
