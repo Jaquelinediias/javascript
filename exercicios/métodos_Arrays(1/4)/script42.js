ca2799ba29fd54f234db9be77357e2dfbda0525e
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-07-12 22:55:56 -0300

    Manipulação de arrays_execercicio

---
+++ exercicios/métodos_Arrays(1/4)/script42.js
@@ -0,0 +1,25 @@
+let lista = ['Ovo', 'Leite', 'Manteiga'];
+
+lista[0] = 'Farinha'
+
+let res = lista;
+  
+       //MANIPULAÇÃO DE ARRAYS//
+
+//length quantidade de item no array
+
+// lista [0] = pega o primeiro item, e (= 'farinha' )substitui o primeiro item do array
+
+//push() Adciona um novo item ao array
+
+//shift() Remove o primeiro item  da lista do array.
+
+//pop() Remove o ultimo item da lista do array.
+
+//indexOf procura um item especifico dentro do  array.
+
+//join('-') separa os itens do array com o divisor que usamos dentro do parêmantro.
+
+//toString() transforma um array em uma string.
+
+console.log(res);
