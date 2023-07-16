38a579c099387de7a177dde57867c5d567c4838e
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-07-16 19:53:32 -0300

    exercício concluido

---
+++ exercicios/métodos_Array_3/4/script.js
@@ -0,0 +1,21 @@
+let lista = [45, 8, 95, 25];
+
+let lista2 = []
+
+/*
+lista2 = lista.map(function(item){
+     return item * 2;
+})
+*/
+
+/*for ( let i in lista ){
+    lista2.push(lista[i] * 2)
+}
+*/
+for (let i = 0; i < 5; i++) {
+    lista.sort()
+    
+}
+let res = lista2;
+
+console.log(res)
