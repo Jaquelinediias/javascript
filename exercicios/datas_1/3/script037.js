8c63ee1dec72312b8baae31a3e29405fe43ef986
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-07-17 21:01:49 -0300

    atividade concluida

---
+++ exercicios/datas_1/3/script037.js
@@ -0,0 +1,45 @@
+//let d = new Date(2023, 7, 1, 17, 50, 2);
+let d = new Date('2020-01-01 15:48:10');
+
+console.log(d.toString())
+
+console.log(d.toUTCString())
+
+// Manipulação de Datas.
+
+let time = new Date()
+
+let novoValor = time
+
+//getSeconds() Pega os segundos
+
+//getMinutes pega os minutos.
+
+//getHours() Pega a hora.
+
+//getDate() a data do dia.
+
+//getDay() Pega o Dia da semana
+
+//getMonth() Pegar o Mês.
+
+//getFullYear() Pega o ano.
+
+//console.log(novoValor);
+
+
+   // MANIPULAÇÃO DE DATAS
+
+let j = new Date()
+
+j.setDate(j.getDate() + 60) // manipula datas.
+
+j.setHours(j.getHours() + 15) // manipula horas.
+
+//setMonth(7) troca o mês.
+
+//setFullYear(2022)  seta o ano, para qualquer que seja.
+
+let valor = j;
+
+console.log(valor)
