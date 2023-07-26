2594db68058a6ab573640e279e962b0c22111f0e
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-07-26 10:51:30 -0300

    pasta nova criada

---
+++ exercicios/Arrow_Functions/script016.js
@@ -0,0 +1,35 @@
+/*function somar(y, x) {
+    return y + x;
+}
+console.log(somar(10, 89));
+*/
+
+// Segunda forma de fazer uma função.
+/*
+let somar  = function(x, y){
+   return x + y;
+}
+*/
+
+// Terceira forma de fazer uma função.
+/*
+let somar =(x, y) => {
+    return x + y;
+}
+*/
+
+/*
+let somar = (x, y) => x + y;
+*/
+
+/*let letrasNome = (nome) => {
+    return nome.length;
+}
+*/
+let letrasNome = (nome) => nome.length;
+
+console.log(letrasNome('JaquelineDias'));
+
+// A unica diferença entre uma função normal e uma anônima(arrow), a gente não tem o objeto This.
+
+
