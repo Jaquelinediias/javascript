1e5ea3434045b232952e80254f712fbab3602780
Jaqueline Da Conceição Dias <128565143+Jaquelinediias@users.noreply.github.com>
2023-08-17 10:12:22 -0300

    exercicio criado

--- MiniProjetos/compraPizza/script.js
+++ MiniProjetos/compraPizza/script.js
@@ -5,9 +5,17 @@
 const j = (el)=>document.querySelector(el);
 const js = (el)=>document.querySelectorAll(el);
 
-pizzaJson.map((pizza, index)=>{
-   let pizzaItem =j('.models .pizza-item').cloneNode(true);
-   
+pizzaJson.map((item, index)=>{
+   let pizzaItem = j('.models .pizza-item').cloneNode(true);
+   // preenche as informações em pizzaItem
+
+   pizzaItem.querySelector('.pizza-item--img').src = item.img;
+   pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+   pizzaItem.querySelector('.pizza-item--price').innerHTML = `RS ${item.price.toFixed(2)}`;
+   pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+
+  
+
    j('.pizza-area').append( pizzaItem);
 });
 
